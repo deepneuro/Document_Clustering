a01d588d919e78e9924c1b72df0bd8615faf48bd
Sérgio Jesus <up201305552@fe.up.pt>
2018-10-08 14:39:52 +0100

    Deletion of files

--- add_PYTHONPATH.py
+++
@@ -1,6 +0,0 @@
-#%%
-import sys
-sys.path.insert(0, "/home/emanuel/Desktop/Doc_Clustering/Document_Clustering")
-for x in sys.path:
-    print(x)
-
--- detect_language.py
+++
@@ -1,4 +0,0 @@
-class DetectLanguage():
-
-    def __init__(self):
-        pass
